904c48aaea0a8aa2b2bcdbd5abb9465f6738f894
Akash Kumar Singh <singhakashkumar101@gmail.com>
2024-01-01 00:45:51 +0530

    Final Code
    chat app deployment

---
+++ Client/build/precache-manifest.7b25b312992aba83050af689f02c45ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "319f75af81540f57067544233d595644",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c369d524a41fb5c5f13a",
+    "url": "/static/css/main.5d41ca5f.chunk.css"
+  },
+  {
+    "revision": "316311dd4d60d15b32da",
+    "url": "/static/js/2.6efa9259.chunk.js"
+  },
+  {
+    "revision": "c369d524a41fb5c5f13a",
+    "url": "/static/js/main.c14d3b43.chunk.js"
+  },
+  {
+    "revision": "20dcb6334915f836950a",
+    "url": "/static/js/runtime~main.00070426.js"
+  }
+]);
